db3c6acb1210276addb5d6d58f5016a3b6ab6091
harsh-d12 <dhobaleharshwardhan.cse24@sbjit.edu.in>
2026-04-03 19:35:05 +0530

    Create 08.java

---
+++ 08.java
@@ -0,0 +1,15 @@
+class TextProcessingBuilder {
+    public static void main(String[] args) {
+        
+        // Create a StringBuilder
+        StringBuilder sb = new StringBuilder("Hello");
+
+        // Append additional text
+        sb.append(" World!");
+
+        // Display modified string
+        System.out.println("Modified String: " + sb);
+
+  
+    }
+}
